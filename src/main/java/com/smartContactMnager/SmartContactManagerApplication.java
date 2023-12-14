91b34664bdf7e04522c88cd18b1ba7aa689ff9b5
aakash0031 <aakashbittu004@gmail.com>
2023-12-14 17:46:19 +0530

    smart contact added to the repo

--- src/main/java/com/smartContactMnager/SmartContactManagerApplication.java
+++ src/main/java/com/smartContactMnager/SmartContactManagerApplication.java
@@ -2,9 +2,10 @@ package com.smartContactMnager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartContactManagerApplication {
+public class SmartContactManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
